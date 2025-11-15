5c42265935691b383b4ea2fbdebe90980f66a4e6
HaruK0720 <haru26720@gmail.com>
2025-11-15 11:28:45 +0900

    commit today's learning

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,3 @@
 'use strict'; // これは厳格モードを有効にするための宣言です。 strictモードでは、より厳格なエラーチェックが行われ、潜在的なバグを防ぐことができます。
 
-console.log('Hello World! from main.js');
+console.log('Hello World! from main.js'); // コンソールに "Hello World! from main.js" と表示します。 tokenとは、セミコロン 大なり小なり記号 括弧といった文字を指している。
