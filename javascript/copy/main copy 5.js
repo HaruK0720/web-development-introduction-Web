798e05e5eab94c6dc5b2377e6ba632b7d24f4672
HaruK0720 <haru26720@gmail.com>
2025-11-21 13:47:49 +0900

    commit today's learning ++

---
+++ javascript/copy/main copy 5.js
@@ -0,0 +1,11 @@
+'use strict';
+// console.log('Hello! I'm Haru Kidachi.); // エラー
+console.log('Hello! I\m Haru Kidachi.'); // エスケープシーケンス →mac: option + ¥, windows: ¥ 
+
+console.log('\¥ \t AAA \n BBB'); // \t: タブ, |n: 改行
+
+console.log("Hello! I'm Haru Kidachi.");
+console.log('"Kidachi"-san.');
+
+console.log('Hello');
+console.log("Hello");
--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,29 @@
 'use strict';
-// console.log('Hello! I'm Haru Kidachi.); // エラー
-console.log('Hello! I\m Haru Kidachi.'); // エスケープシーケンス →mac: option + ¥, windows: ¥ 
 
-console.log('\¥ \t AAA \n BBB'); // \t: タブ, |n: 改行
+let num = 10;
 
-console.log("Hello! I'm Haru Kidachi.");
-console.log('"Kidachi"-san.');
+// インクリメント
+num++;
+console.log(num); // 11
 
-console.log('Hello');
-console.log("Hello");
+// デクリメント
+num--;
+console.log(num); // 10
+
+// 前置演算子のインクリメント
+++num;
+console.log(num); // 11
+
+// 前置演算子のデクリメント
+--num;
+console.log(num); // 10;
+
+console.log('---');
+
+let num1 = 10;
+let num2 = 10;
+let ans1 = ++num1; // num1を先にインクリメントしてからans1に代入
+let ans2 = num2++; // ans2にnum2を代入してからnum2をインクリメント
+
+console.log(ans1); // 11
+console.log(ans2); // 10
