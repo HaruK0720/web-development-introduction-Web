0969c405f534f12fda2b06190d182ae07575a294
HaruK0720 <haru26720@gmail.com>
2025-12-04 12:53:16 +0900

    commit today's studying array

---
+++ javascript/copy/main 16 array.js
@@ -0,0 +1,11 @@
+'use strict';
+const colors = ['Red', 'Green', 'Blue'];
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+// console.log(colors[3]); // undefined
+
+// console.log(colors.length); // 配列の要素数を取得 (デバッグ用)
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
