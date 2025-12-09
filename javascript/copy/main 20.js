a9f5be09dc4b23e812556977dfcb7875b859653f
HaruK0720 <haru26720@gmail.com>
2025-12-09 17:58:47 +0900

    commit today's studying function literal

---
+++ javascript/copy/main 20.js
@@ -0,0 +1,15 @@
+'use strict';
+// 関数リテラル
+const getRectangle = function(height, width) {
+    return height * width;
+}
+
+console.log(getRectangle(3, 5));
+
+
+// 前回のfunction命令文
+// function getRectangle(height, width) {
+//     return height * width;
+// }
+
+// console.log(getRectangle(3, 5));
