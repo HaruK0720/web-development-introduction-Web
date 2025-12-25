3bc30860325c0a81c920d28bee4462043d12d50b
HaruK0720 <haru26720@gmail.com>
2025-12-25 17:03:45 +0900

    commit today's studying load

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,1 @@
 'use strict';
-const e = document.getElementById('button');
-e.onclick = () => {
-    console.log('Clicked!')
-}
-
-
-
-// 構文：
-// オブジェクト名.onイベント名 = () => {
-//     // イベントハンドラ
-// }
