4045fb56b354eb7920991fcae3771c60636af077
HaruK0720 <haru26720@gmail.com>
2026-01-01 17:25:23 +0900

    commit today's studying nodejs

---
+++ nodejs_project/hello_world.js
@@ -0,0 +1,12 @@
+console.log('Hello World from file!');
+console.log(2 + 3);
+
+// cd nodejs_project
+// node hello_world.js
+
+
+// 面倒だけどしっかりパス指定 ↓
+// haru_k@HaruKMacBook-Air web-development-introduction-Web % node nodejs_project/hello_world.js
+// Hello World from file!
+// 5
+// haru_k@HaruKMacBook-Air web-development-introduction-Web % 
